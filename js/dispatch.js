bf9eb6db411c5418f6858016d729e8208aa8a87d
leomcelroy <leomcelroy@gmail.com>
2021-09-10 15:43:43 -0400

    switched drag modifier to use proper js ast

--- js/dispatch.js
+++ js/dispatch.js
@@ -11,6 +11,8 @@ import { Turtle } from "./Turtle.js";
 import { parse2 } from "./parser.js";
 import esprima from 'https://cdn.skypack.dev/esprima';
 import acorn from 'https://cdn.skypack.dev/acorn';
+import { generate } from 'https://cdn.skypack.dev/astring';
+import { walk } from 'https://cdn.skypack.dev/esprima-walk';
 
 
 const STATE = {
@@ -121,37 +123,59 @@ const ACTIONS = {
 		dispatch("RENDER");
 	},
 	TRANSLATE({ x, y, index }, state) {
-		// console.log(x, y, index);
+		// // console.log(x, y, index);
+		// const string = state.codemirror.view.state.doc.toString();
+		// const asts = parse2(string);
+		// const { index: startI, ast } = asts[index];
+		// // find translate
+		// const translateTerm = ast.filter(line => line[0].value === "translate")[0];
+		// const firstBrack = translateTerm[2][0].index + startI + 1;
+		// const secondBrack = translateTerm[2][2].index + startI;
+		// const from = translateTerm[2][0].index + startI;
+		// const comp = state.storedPCB.components[index];
+		// const round = num => Math.round(num*10000)/10000;
+		// const insert = `${round(x + comp.posX)}, ${round(y + comp.posY)}`;
+		// const to = from + insert.length;
+		// state.codemirror.view.dispatch({
+		// 	changes: { from: firstBrack, to: secondBrack, insert }
+		// });
+
 		const string = state.codemirror.view.state.doc.toString();
-
-		// const stringToParse = `()=>{${string}}`;
-		// const esprimaAST = esprima.parseScript(stringToParse);
-		// console.log(acorn);
-		// const acornAST = acorn.parse(stringToParse, {ecmaVersion: 2020})
-		// console.log("asprimaAST:", esprimaAST);
-		// console.log("acornAST:", acornAST);
-
-		// console.time()
-		const asts = parse2(string);
-		// console.timeEnd()
-
-		const { index: startI, ast } = asts[index];
-
-		// find translate
-		const translateTerm = ast.filter(line => line[0].value === "translate")[0];
-		// console.log(translateTerm);
-		const firstBrack = translateTerm[2][0].index + startI + 1;
-		const secondBrack = translateTerm[2][2].index + startI;
-		const from = translateTerm[2][0].index + startI;
-		const comp = state.storedPCB.components[index];
+		const stringToParse = `()=>{${string}}`; // remember to subtract 5 from indices
+		const esprimaAST = esprima.parseScript(stringToParse, { range: true });
+		const mainBody = esprimaAST.body[0].expression.body.body;
+		let adds = [];
+		walk(esprimaAST, node => {
+			try {
+		  		if (node.callee.type === "MemberExpression" && node.callee.property.name === "add") adds.push(node.arguments[1]);
+			} catch (err) { }
+		})
+		// sort by first range
+		const sortedAdds = adds.sort((a, b) => a.range[0] - b.range[0])
 		const round = num => Math.round(num*10000)/10000;
-		const insert = `${round(x + comp.posX)}, ${round(y + comp.posY)}`;
-		const to = from + insert.length;
-		// console.log(string.slice(firstBrack, secondBrack));
+		const comp = state.storedPCB.components[index];
+		try {
+			sortedAdds[index].properties.forEach( prop => {
+				if (prop.key.name !== "translate") return;
+				prop.value.elements = [
+					{type: 'Literal', value: round(x + comp.posX), raw: `${round(x + comp.posX)}`},
+					{type: 'Literal', value: round(y + comp.posY), raw: `${round(y + comp.posY)}`}
+				]
+			})
+		} catch (err) {}
 
 
+		// modify values here
+		// find adjustable parameter
+		// should be first number in expression
+		// replace value with new value
+
+		// generate
+		const insert = generate(sortedAdds[index]);
+
+		// reinsert
 		state.codemirror.view.dispatch({
-			changes: { from: firstBrack, to: secondBrack, insert }
+			changes: { from: sortedAdds[index].range[0] - 5, to: sortedAdds[index].range[1] - 5, insert }
 		});
 
 		dispatch("RUN");
--- js/events.js
+++ js/events.js
@@ -49,15 +49,20 @@ export function addEvents(state) {
 		if (code === "Enter" && event.shiftKey) {
 		  event.preventDefault();
 		  dispatch("RUN");
-
+		} else if (code === "KeyT" && event.shiftKey) {
+      
       const string = state.codemirror.view.state.doc.toString();
       const stringToParse = `()=>{${string}}`; // remember to subtract 5 from indices
+
       const esprimaAST = esprima.parseScript(stringToParse, { range: true });
       // console.log("esprimaAST:", esprimaAST);
+
       const mainBody = esprimaAST.body[0].expression.body.body;
       // console.log(mainBody);
-      const newScript = { type: 'Program', body: mainBody, sourceType: 'script' };
+
+      // const newScript = { type: 'Program', body: mainBody, sourceType: 'script' };
       // console.log(generate(newScript));
+
       let adds = [];
       walk(esprimaAST, node => {
         try {
@@ -67,17 +72,21 @@ export function addEvents(state) {
 
       // sort by first range
       const sortedAdds = adds.sort((a, b) => a.range[0] - b.range[0])
+      console.log(sortedAdds);
 
       // modify values here
+      // find adjustable parameter
+      // should be first number in expression
+      // replace value with new value
 
       // generate
-      console.log(generate(sortedAdds[0]));
+      console.log(generate(sortedAdds[1]));
 
       // reinsert
-		} else if (code === "KeyT" && event.shiftKey) {
-      console.time()
-      dispatch("RENDER")
-      console.timeEnd()
+      state.codemirror.view.dispatch({
+        changes: { from: sortedAdds[1].range[0] - 5, to: sortedAdds[1].range[1] - 5, insert: generate(sortedAdds[1]) }
+      });
+
     }
 	})
 
