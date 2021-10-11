066cf3f7c084883b5b1971e2e1120d81c8a812ed
leomcelroy <leomcelroy@gmail.com>
2021-10-11 14:37:57 +0000

    Deploying to gh-pages from @ leomcelroy/svg-pcb@88f718d2ad84b6ab13c1a95244834c75033ecc5a 🚀

--- build/snowpack.config.js
+++
@@ -1,9 +0,0 @@
-export default {
-  alias: {
-    "lit-html": "https://cdn.skypack.dev/lit-html",
-    esprima: "https://cdn.skypack.dev/esprima",
-    acorn: "https://cdn.skypack.dev/acorn",
-    astring: "https://cdn.skypack.dev/astring",
-    "esprima-walk": "https://cdn.skypack.dev/esprima-walk"
-  }
-};
