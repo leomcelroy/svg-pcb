702ddb5f9796da2ec3d665a53d04f48b2c73696d
qbolsee <quentinbolsee@hotmail.com>
2021-10-11 15:25:47 +0000

    Deploying to gh-pages from @ leomcelroy/svg-pcb@10003b8ca6e2ef9d6112541ec0fbf3ece6116a8c 🚀

--- examples/component.js
+++ examples/component.js
@@ -1,41 +1,25 @@
-const SOIC14 = (() => { return kicadToObj(
-`(module fab-SOIC-14_3.9x8.7mm_P1.27mm (layer F.Cu) (tedit 5EABE9B1)
-  (descr "SOIC, 14 Pin, fab version")
-  (tags "SOIC fab")
-  (attr smd)
-  (fp_text reference REF** (at 0 -5.28) (layer F.SilkS)
-    (effects (font (size 1 1) (thickness 0.15)))
-  )
-  (fp_line (start 0 4.435) (end 1.95 4.435) (layer F.SilkS) (width 0.12))
-  (fp_line (start 0 4.435) (end -1.95 4.435) (layer F.SilkS) (width 0.12))
-  (fp_line (start 0 -4.435) (end 1.95 -4.435) (layer F.SilkS) (width 0.12))
-  (fp_line (start 0 -4.435) (end -3.45 -4.435) (layer F.SilkS) (width 0.12))
-  (fp_line (start -0.975 -4.325) (end 1.95 -4.325) (layer F.Fab) (width 0.1))
-  (fp_line (start 1.95 -4.325) (end 1.95 4.325) (layer F.Fab) (width 0.1))
-  (fp_line (start 1.95 4.325) (end -1.95 4.325) (layer F.Fab) (width 0.1))
-  (fp_line (start -1.95 4.325) (end -1.95 -3.35) (layer F.Fab) (width 0.1))
-  (fp_line (start -1.95 -3.35) (end -0.975 -4.325) (layer F.Fab) (width 0.1))
-  (pad 1 smd rect (at -2.475 -3.81) (size 1.5 0.76) (layers F.Cu F.Paste F.Mask))
-  (pad 2 smd rect (at -2.475 -2.54) (size 1.5 0.76) (layers F.Cu F.Paste F.Mask))
-  (pad 3 smd rect (at -2.475 -1.27) (size 1.5 0.76) (layers F.Cu F.Paste F.Mask))
-  (pad 4 smd rect (at -2.475 0) (size 1.5 0.76) (layers F.Cu F.Paste F.Mask))
-  (pad 5 smd rect (at -2.475 1.27) (size 1.5 0.76) (layers F.Cu F.Paste F.Mask))
-  (pad 6 smd rect (at -2.475 2.54) (size 1.5 0.76) (layers F.Cu F.Paste F.Mask))
-  (pad 7 smd rect (at -2.475 3.81) (size 1.5 0.76) (layers F.Cu F.Paste F.Mask))
-  (pad 8 smd rect (at 2.475 3.81) (size 1.5 0.76) (layers F.Cu F.Paste F.Mask))
-  (pad 9 smd rect (at 2.475 2.54) (size 1.5 0.76) (layers F.Cu F.Paste F.Mask))
-  (pad 10 smd rect (at 2.475 1.27) (size 1.5 0.76) (layers F.Cu F.Paste F.Mask))
-  (pad 11 smd rect (at 2.475 0) (size 1.5 0.76) (layers F.Cu F.Paste F.Mask))
-  (pad 12 smd rect (at 2.475 -1.27) (size 1.5 0.76) (layers F.Cu F.Paste F.Mask))
-  (pad 13 smd rect (at 2.475 -2.54) (size 1.5 0.76) (layers F.Cu F.Paste F.Mask))
-  (pad 14 smd rect (at 2.475 -3.81) (size 1.5 0.76) (layers F.Cu F.Paste F.Mask))
-  (model {KISYS3DMOD}/Package_SO.3dshapes/SOIC-14_3.9x8.7mm_P1.27mm.wrl
-    (at (xyz 0 0 0))
-    (scale (xyz 1 1 1))
-    (rotate (xyz 0 0 0))
-  )
-)
-`)})()
+const test_footprint = {
+  "VCC": {
+    "shape": "M -0.05 0.025L 0.05 0.025L 0.05 -0.025L -0.05 -0.025L -0.05 0.025",
+    "pos":[-0.1,0.05],
+    "layers":["F.Cu"]
+  },
+  "GND": {
+    "shape": "M -0.05 0.025L 0.05 0.025L 0.05 -0.025L -0.05 -0.025L -0.05 0.025",
+    "pos":[0.1,0.05],
+    "layers":["F.Cu"]
+  },
+  "D+": {
+    "shape": "M -0.05 0.025L 0.05 0.025L 0.05 -0.025L -0.05 -0.025L -0.05 0.025",
+    "pos":[-0.1,-0.05],
+    "layers":["F.Cu"]
+  },
+  "D-": {
+    "shape": "M -0.05 0.025L 0.05 0.025L 0.05 -0.025L -0.05 -0.025L -0.05 0.025",
+    "pos":[0.1,-0.05],
+    "layers":["F.Cu"]
+  }
+}
 
 // press shift+enter to run
 
@@ -47,7 +31,7 @@ const SOIC14 = (() => { return kicadToObj(
 
 let board = new PCB();
 
-d11c = board.add(SOIC14, {translate: [0.43, 0.72], name: "TEST"})
+test_comp = board.add(test_footprint, {translate: [0.5, 0.5], name: "COMP"})
 
 return {
   shapes: [
