554543920d39bcaf1e092d71047aab99249ff410
leomcelroy <leomcelroy@gmail.com>
2021-10-17 16:56:06 -0400

    added import map

--- js/main.js
+++ js/main.js
@@ -1,9 +1,5 @@
 import { dispatch } from "./dispatch.js";
 
-export { PCB } from "./pcb.js";
-export { kicadToObj } from "./ki_cad_parser.js"
-export { Turtle } from "./Turtle.js";
-
 window.dispatch = dispatch;
 
 window.addEventListener("load", () => {
