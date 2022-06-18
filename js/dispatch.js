092bdeb12fc00e0d965ffda0591eaefba6759c9f
leomcelroy <leomcelroy@gmail.com>
2022-06-18 01:04:45 -0400

    Update dispatch.js

--- js/dispatch.js
+++ js/dispatch.js
@@ -267,6 +267,11 @@ const ACTIONS = {
 
 export function dispatch(action, args = {}) {
 	const trigger = ACTIONS[action];
-	if (trigger) return trigger(args, STATE);
+	if (trigger) {
+		console.time(action);
+		const result = trigger(args, STATE);
+		console.timeEnd(action);
+		return result;
+	}
 	else console.log("Action not recongnized:", action);
 }
