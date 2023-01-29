3141c8ff8053ed140dd3ab9c7861ccab35a21729
Krisjanis Rijnieks <krisjanis.rijnieks@gmail.com>
2023-01-29 19:43:47 +0200

    Move nested loop to snapToPad function

--- js/events/addPathManipulation.js
+++ js/events/addPathManipulation.js
@@ -45,29 +45,12 @@ export function addPathManipulation(state, svgListener) {
 
     const targetPoint = svgPoint(clickedPoint);
 
-    // Make this mutable so we can use other filters later
     let pt = {
       x: snapToGrid(targetPoint.x),
       y: snapToGrid(targetPoint.y)
     }
 
-    // BEGIN: Snap to pad
-    const components = state.pcb.components;
-    for (const comp in components) {
-      const pads = components[comp].pads;
-      for (const pad in pads) {
-        const p = pads[pad];
-        const dx = Math.abs(targetPoint.x - p[0]);
-        const dy = Math.abs(targetPoint.y - p[1]);
-        if (dx < 0.05 && dy < 0.05) {
-          console.log(`Nearby pad ${pad}`);
-          pt.x = snapToPad(p[0], pt.x);
-          pt.y = snapToPad(p[1], pt.y);
-          break;
-        }
-      }
-    }
-    // END: Snap to pad
+    pt = snapToPad(pt);
 
     const doc = state.codemirror.view.state.doc;
     const string = doc.toString();
--- js/snapToPad.js
+++ js/snapToPad.js
@@ -1,6 +1,22 @@
 import { global_state } from "./global_state.js";
 
-export const snapToPad = (valPad, valInit) => 
-   !global_state.snapPad 
-     ? parseFloat(valInit).toFixed(3) 
-     : parseFloat(valPad).toFixed(3);
+export const snapToPad = function(pt) {
+  if (!global_state.snapPad) return pt;
+
+  const components = global_state.pcb.components;
+  for (const comp in components) {
+    const pads = components[comp].pads;
+    for (const pad in pads) {
+      const p = pads[pad];
+      const dx = Math.abs(pt.x - p[0]);
+      const dy = Math.abs(pt.y - p[1]);
+      if (dx < 0.05 && dy < 0.05) {
+        pt.x = parseFloat(p[0]).toFixed(3);
+        pt.y = parseFloat(p[1]).toFixed(3);
+        break;
+      }
+    }
+  }
+
+  return pt;
+};
