5460bda8db29b06ebd9a10382e11b58db43450c9
leomcelroy <leomcelroy@gmail.com>
2023-02-19 21:46:39 -0500

    removed PathKit

--- js/main.js
+++ js/main.js
@@ -1,23 +1,6 @@
 import { init } from "./init.js";
-import PathKitInit from '../PathKit/pathkit.js';
 
 
 window.addEventListener("load", async () => {
-	// const PathKit = await PathKitInit({ 
-	// 	locateFile: (file) => {
-	// 		return "../PathKit/" + file 
-	// 	}
-	// });
-
-	// console.log(PathKit);
-
-	// let path = PathKit.FromSVGString('M150 0 L75 200 L225 200 Z');
-	// let pathOne = PathKit.NewPath().moveTo(0, 20).lineTo(10, 10).lineTo(20, 20).close();
-	// let pathTwo = PathKit.NewPath().moveTo(10, 20).lineTo(20, 10).lineTo(30, 20).close();
-	// let mountains = PathKit.MakeFromOp(path, pathTwo, PathKit.PathOp.UNION);
-	
-	// console.log(mountains.toCmds());
-	// console.log(mountains.toSVGString());
-
 	init();
 });
