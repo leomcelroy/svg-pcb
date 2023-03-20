47f5aa2676a3a6856cefa284ad7a355bc3835376
Krisjanis Rijnieks <krisjanis.rijnieks@gmail.com>
2023-03-20 21:12:17 +0200

    Update snapToPad: replace if with tertiary operator (leo style)

--- js/snapToPad.js
+++ js/snapToPad.js
@@ -20,17 +20,10 @@ export const snapToPad = function(pt) {
       // This selection area is off.
       // Best would be to compare against the actual shape of the pad.
       if (Math.sqrt(dx**2 + dy**2) < global_state.snapToPadRadius) {
-
         pt.x = parseFloat(p[0]).toFixed(3);
         pt.y = parseFloat(p[1]).toFixed(3);
         pt.snapped = true;
-
-        if (refDes !== "") {
-          pt.padRef = `${refDes}.pad("${pad}")`;
-        } else {
-          pt.padRef = `pt(${pt.x}, ${pt.y})`;
-        }
-        
+        pt.padRef = (refDes !== "") ? `${refDes}.pad("${pad}")` : `pt(${pt.x}, ${pt.y})`;
         break;
       }
     }
