d2913e13e1ceb985d16363623095e66552d9e27b
Krisjanis Rijnieks <krisjanis.rijnieks@gmail.com>
2023-03-23 17:00:10 +0200

    Add GerberBuilder class for developer friendlier gerber export. It now includes solder mask support

--- js/events/downloadGerber.js
+++ js/events/downloadGerber.js
@@ -4,24 +4,177 @@ import JSZip from "jszip";
 import { saveAs } from "file-saver";
 import { global_state } from "../global_state.js";
 
-function toMM(inches){
-    return inches * global_state.mm_per_unit;
+// This should be a global function
+function inchesToMM(inches){
+  return inches * 25.4;
 }
 
+// This is here only for the drills implementation
 function format(x) {
-    let s = x.toFixed(6); // Reduce decimal digits to 6 (0.123456)
-    s = s.replace(".", ""); // Remove the floating point (0.123456 becomes 0123456)
+  // Reduce decimal digits to 6 (0.123456)
+  let s = x.toFixed(6);
+
+  // Remove the floating point (0.123456 becomes 0123456)
+  s = s.replace(".", ""); 
+
+  // Remove leading zeroes for Gerberization
+  while (s.startsWith("0")) {
+    s = s.substr(1, s.length);
+  }
+
+  return s;
+}
+
+class GerberBuilder {
+  #body = '';
+  #apertureConter = 10; // use getApertureID() not this directly
+  #wireThicknesses = [];
+  
+  constructor() {}
+
+  #format(x) {
+    // Reduce decimal digits to 6 (0.123456)
+    let s = x.toFixed(6);
+
+    // Remove the floating point (0.123456 becomes 0123456)
+    s = s.replace(".", ""); 
 
     // Remove leading zeroes for Gerberization
     while (s.startsWith("0")) {
-        s = s.substr(1, s.length);
+      s = s.substr(1, s.length);
     }
 
     return s;
+  }
+
+  #getHeader() {
+    let str = '';
+      
+    // Add date and time of generation.
+    const dateTime = new Date().toISOString();
+    str += "%TF.CreationDate," + dateTime + "*%\n";
+
+    // Add a header comment at the top to indicate the generator.
+    // This could make use of predefined global constants later.
+    str += "%TF.GenerationSoftware,Leo McElroy & Contributors,SvgPcb,v0.1*%\n";
+
+    // It is recommended to use metric. Imperial is there for historic reasons only 
+    // and will be deprecated at a future date. Source: Gerber Spec. p. 46.
+    str += "%MOMM*%\n";
+
+    // 4 integer digits and 6 decimal digits, borrowed from KiCad gerber export.
+    str += "%FSLAX46Y46*%\n";
+
+    return str;
+  }
+
+  #getFooter() {
+    return "M02*";
+  }
+
+  // Helper function to automate aperture ID counting
+  #getApertureID() {
+    const apertureID = this.#apertureConter;
+    this.#apertureConter++;
+    return apertureID;
+  }
+
+  plotWires(layer) {
+    // Declare apertures
+    let wires = [];
+    layer.map( el => {
+      if (el.type !== "wire") return;
+
+      const wireThickness = inchesToMM(el.thickness).toFixed(3);
+        
+      // Add aperture only if it does not exist yet
+      if (!this.#wireThicknesses.includes(wireThickness)) {
+        this.#wireThicknesses.push(wireThickness);
+        const apertureID = this.#getApertureID();
+        this.#body +="%ADD" + apertureID.toString() +  "C," + wireThickness.toString() + "*%\n";
+      }
+
+      // Add modified wire object to array
+      el.gerberAperture = this.#wireThicknesses.indexOf(wireThickness) + (this.#apertureConter - this.#wireThicknesses.length);
+      wires.push(el); 
+    });
+
+    // Draw wires
+    wires.map( el => {
+      // Draw the wire using an aperture
+      this.#body += "D" + el.gerberAperture.toString() + "*\n";
+      for (let i = 0; i < el.shape[0].length; i++) {
+        const x = this.#format( inchesToMM(el.shape[0][i][0]) );
+        const y = this.#format( inchesToMM(el.shape[0][i][1]) );
+        this.#body += "X" + x + "Y" + y + "D0" + (i === 0 ? 2 : 1) + "*\n";
+      }
+    });
+  } // End of GerberBuilder class
+
+  // Plot pads in a gerber file.
+  // This method can be also used to produce solder mask with a certain offset.
+  // Offest is defined in mm, 0.1mm for example.
+  plotPads(layer, offset = 0) {
+    const apertureDiameter = offset ? offset * 2 : 0;
+    const apertureID = this.#getApertureID();
+
+    // Define aperture
+    this.#body += "%ADD" + apertureID.toString() +  "C," + apertureDiameter.toFixed(3) + "*%\n";
+
+     // Select the aperture for drawing the outline
+    this.#body += "D" + apertureID.toString() + "*\n";
+
+     // Enable linear interpolation 
+    this.#body += "G01*\n";
+
+    // Draw pads as polygons
+    layer.map( el => {
+      if (el.type === "wire") return;
+      
+      this.#body += "G36*\n"; 
+      
+      for (let i = 0; i < el[0].length; i++) {
+        const x = this.#format( inchesToMM(el[0][i][0]) );
+        const y = this.#format( inchesToMM(el[0][i][1]) );
+        this.#body += "X" + x + "Y" + y + "D0" + (i === 0 ? 2 : 1) + "*\n";
+      }
+      
+      this.#body += "G37*\n";
+    });
+  }
+
+  plotOutline(layer) {
+    const apertureDiameter = 0.1;
+    const apertureID = this.#getApertureID();
+
+    // Define aperture
+    this.#body += "%ADD" + apertureID.toString() +  "C," + apertureDiameter.toFixed(3) + "*%\n";
+
+     // Select the aperture for drawing the outline
+    this.#body += "D" + apertureID.toString() + "*\n";
+    
+    // Enable linear interpolation 
+    this.#body += "G01*\n";
+      
+    // Plot outling
+    layer.map( el => {
+      for (let i = 0; i < el[0].length; i++) {
+        const x = this.#format( inchesToMM(el[0][i][0]) );
+        const y = this.#format( inchesToMM(el[0][i][1]) );
+        this.#body += "X" + x + "Y" + y + "D0" + (i === 0 ? 2 : 1) + "*\n";
+      }
+    });
+  }
+
+  toString() {
+    let str = this.#getHeader();
+    str += this.#body;
+    str += this.#getFooter();
+    return str;
+  }
 }
 
 export function downloadGerber(state) {
-
     // Some Gerber tips.
     // Include outline in all layers to avoid align issues.
     // Use flashed pads wherever possible.
@@ -45,19 +198,7 @@ export function downloadGerber(state) {
 
     const layers = state.pcb.layers;
   
-    // It is possible that one could draw the wire with Gerber apertures.
-    // Will look into that.
-    const expandWire = w => offset2(
-      w.shape, 
-      w.thickness/2, 
-      {
-        endType: "etOpenRound", 
-        jointType:"jtRound", 
-      }
-    );
-  
     const drill = (layers["drill"] ? layers["drill"] : []).flat().map( x => {
-  
       const getCenter = (pts) => {
         let totalX = 0;
         let totalY = 0;
@@ -70,7 +211,6 @@ export function downloadGerber(state) {
       }
   
       const getDistance = (pt0, pt1) => Math.sqrt((pt1[0] - pt0[0])**2+(pt1[1] - pt0[1])**2);
-  
       const center = getCenter(x);
       const dist = Math.round(1000*x.reduce((acc, cur) => acc + getDistance(center, cur), 0)/x.length)/1000;
   
@@ -80,193 +220,6 @@ export function downloadGerber(state) {
       };
     });
 
-    /* START: reusable gerber macro functions */
-
-    const getGerberHeader = () => {
-      let str = '';
-      
-      // Add date and time of generation.
-      const dateTime = new Date().toISOString();
-      str += "%TF.CreationDate," + dateTime + "*%\n";
-
-      // Add a header comment at the top to indicate the generator.
-      // This could make use of predefined global constants later.
-      str += "%TF.GenerationSoftware,LeoMcRoy&Co,SvgPcb,v0.1*%\n";
-
-      // It is recommended to use metric. Imperial is there for historic reasons only 
-      // and will be deprecated at a future date. Source: Gerber Spec. p. 46.
-      str += "%MOMM*%\n";
-
-      // 4 integer digits and 6 decimal digits, borrowed from KiCad gerber export.
-      str += "%FSLAX46Y46*%\n";
-
-      return str;
-    };
-
-    const getGerberEdgeCuts = (layer, apertureID) => {
-      let str = '';
-
-      // Define circle with 0.1mm diameter aperture
-      str += "%ADD" + apertureID.toString() +  "C,0.10*%\n";
-
-      // Select the aperture for drawing the outline
-      str += "D" + apertureID.toString() + "*\n";
-
-      // Enable linear interpolation 
-      str += "G01*\n";
-      
-      // Draw Edge Cuts
-      const interiorStrs = layer.map( pts => {
-        let ptsString = pts.reduce((acc, cur, i) => `${acc}X${format( toMM(cur[0]) )}Y${format( toMM(cur[1]) )}D0${i === 0 ? 2 : 1}*\n`, "*\n") 
-        return ptsString;
-      });
-  
-      str += interiorStrs.join("");
-
-      return str;
-    };
-
-    // Circle aperture only for now
-    const getGerberDefineAperture = (apertureID, diameter) => { 
-      return "%ADD" + apertureID.toString() +  "C," + diameter.toString() + "*%\n"; 
-    }; 
-    
-    // Select predefined aperture
-    const getGerberSelectAperture = (apertureID) => {
-      return "D" + apertureID.toString() + "*\n";
-    };
-
-    const getGerberPolarityDark = () => { return "%LPD*%\n"; }
-    const getGerberLinearInerpolation = () => { return "G01*\n"; }
-    const getGerberEndOfFile = () => { return "M02*"; }
-
-    /* END: reusable gerber macro functions */
-    /* START: gerber file generators */
-
-    const makeGerberFile = (layer) => {
-      let apertureCounter = 10;
-      let str = '';
-      str += getGerberHeader();
-
-      // Add zero aperture for polygon shapes
-      const zeroApertureID = apertureCounter;
-      apertureCounter++;
-      str +="%ADD" + zeroApertureID.toString() +  "C,0.0*%\n";
-  
-      // 1ST PASS
-      // Loop through layer and set up apertures for different wire thicknesses and pads.
-      let wireApertureDiameters = [];
-      let wires = [];
-      let shapes = [];
-      layer.map( el => {
-        if (el.type === "wire") {
-          const wireThickness = toMM(el.thickness).toFixed(3);
-          
-          // Add aperture only if it does not exist yet
-          if (!wireApertureDiameters.includes(wireThickness)) {
-            wireApertureDiameters.push(wireThickness);
-            const apertureID = apertureCounter;
-            apertureCounter++;
-            str += getGerberDefineAperture(apertureID, wireThickness);
-          }
-
-          // Add modified wire object to array
-          el.gerberAperture = wireApertureDiameters.indexOf(wireThickness) + (apertureCounter - wireApertureDiameters.length);
-          wires.push(el);
-        } else {
-          // Everything else is a random shape and we define a macro aperture for each.
-          // We do need to do some data transformation here. 
-          // It would be nice to run this data through some kind of analyzer to detect 
-          // a possible shape primitive.
-          el = {
-            shape: el,
-            type: "shape"
-          };
-
-          shapes.push(el);
-        }
-      });
-
-      // Set us up for drawing
-      str += getGerberPolarityDark(); // layer dark
-      str += getGerberLinearInerpolation(); // linear interpolation
-
-      // 2ND PASS
-      // Now we should be able to find aperture indices and draw wires using them
-      // As well as the pads, of course
-      wires.map( el => {
-        // Draw the wire using an aperture
-        str += getGerberSelectAperture(el.gerberAperture);
-        for (let i = 0; i < el.shape[0].length; i++) {
-          const x = format( toMM(el.shape[0][i][0]) );
-          const y = format( toMM(el.shape[0][i][1]) );
-          str += "X" + x + "Y" + y + "D0" + (i === 0 ? 2 : 1) + "*\n";
-        }
-      });
-
-      // Here we draw plain polygons
-      shapes.map( el => {
-        str += getGerberSelectAperture(zeroApertureID);
-        str += "G36*\n"; 
-        for (let i = 0; i < el.shape[0].length; i++) {
-          const x = format( toMM(el.shape[0][i][0]) );
-          const y = format( toMM(el.shape[0][i][1]) );
-          str += "X" + x + "Y" + y + "D0" + (i === 0 ? 2 : 1) + "*\n";
-        }
-        str += "G37*\n";
-      });
-
-      str += getGerberEndOfFile();
-  
-      return str;
-    };
-  
-    const makeFile = (layer, interior = undefined) => {
-      let apertureCounter = 10;
-      let str = '';
-      
-      str += getGerberHeader();
-      str += getGerberPolarityDark(); // layer dark
-      str += getGerberLinearInerpolation(); // linear interpolation
-
-      const zeroApertureID = apertureCounter;
-      apertureCounter++;
-      str +="%ADD" + zeroApertureID.toString() +  "C,0.0*%\n";
-      str += "D" + zeroApertureID.toString() + "*\n";
-
-      // Figure out difference between pads and traces
-
-      const strs = layer.map( pts => {
-        let ptsString = pts.reduce((acc, cur, i) => `${acc}X${format( toMM(cur[0]) )}Y${format( toMM(cur[1]) )}D0${i === 0 ? 2 : 1}*\n`, "G36*\n")
-        ptsString += "G37*\n"; 
-        return ptsString;
-      });
-  
-      str += strs.join("");
-
-      if (interior !== undefined) {
-        const apertureID = apertureCounter;
-        apertureCounter++;
-        str += getGerberEdgeCuts(interior, apertureID);
-      }
-      
-      // End of Gerber file
-      str += getGerberEndOfFile();
-  
-      return str;
-    }
-
-    const makeFileEdgeCuts = (layer) => {
-      let apertureCounter = 10;
-      let str = '';
-      str += getGerberHeader();
-      str += getGerberPolarityDark();
-      str += getGerberLinearInerpolation();
-      str += getGerberEdgeCuts(layer, apertureCounter);
-      str += getGerberEndOfFile();
-      return str;
-    }
-  
     // What format drills here? One could use Gerber for drills too.
     const makeFileDrill = (drills) => {
       const tools = {};
@@ -296,17 +249,9 @@ export function downloadGerber(state) {
          }
       }
       
-      str += "M30\n" // end of program
-  
+      str += "M30\n"; // end of program
       return str;
-    }
-
-    /* END: gerber file generators */
-
-    const interior = 
-      (global_state.downloadGerberOptions.layers.get("Edge.Cuts") || global_state.downloadGerberOptions.includeEdgeCuts) 
-      ? layers["interior"].map( x => { return x; }).flat()
-      : undefined;
+    };
    
     var zip = new JSZip();
     global_state.downloadGerberOptions.layers.forEach((val, key) => {
@@ -314,22 +259,38 @@ export function downloadGerber(state) {
       
       switch (key) {
         case "F.Cu":
-          zip.file(`${state.name === "" ? "anon" : state.name}-F_Cu.gbr`, makeGerberFile(layers["F.Cu"]) );
+          let frontCopper = new GerberBuilder();
+          frontCopper.plotPads(layers["F.Cu"]);
+          frontCopper.plotWires(layers["F.Cu"]);
+          if (global_state.downloadGerberOptions.includeEdgeCuts) {
+            frontCopper.plotOutline(layers["interior"]);
+          }
+          zip.file(`${state.name === "" ? "anon" : state.name}-F_Cu.gbr`, frontCopper.toString() );
           break;
         case "B.Cu":
-          //const backCopper = layers["B.Cu"].map( x => {
-          //  if (x.type === "wire") return expandWire(x);
-          //  else return x;
-          //}).flat();
-          zip.file(`${state.name === "" ? "anon" : state.name}-B_Cu.gbr`, makeGerberFile(layers["B.Cu"]) );
-          //zip.file(`${state.name === "" ? "anon" : state.name}-B_Cu.gbr`, makeFile(backCopper, 
-          //  (global_state.downloadGerberOptions.includeEdgeCuts ? interior : undefined) ));
+          let backCopper = new GerberBuilder();
+          backCopper.plotPads(layers["B.Cu"]);
+          backCopper.plotWires(layers["B.Cu"]);
+          if (global_state.downloadGerberOptions.includeEdgeCuts) {
+            backCopper.plotOutline(layers["interior"]);
+          }
+          zip.file(`${state.name === "" ? "anon" : state.name}-B_Cu.gbr`, backCopper.toString() );
           break;
         case "F.Mask":
-          // To make this possible we need to get the front layer pads and offset them a bit.
+          let frontMask = new GerberBuilder();
+          frontMask.plotPads(layers["F.Cu"], 0.1);
+          if (global_state.downloadGerberOptions.includeEdgeCuts) {
+            frontMask.plotOutline(layers["interior"]);
+          }
+          zip.file(`${state.name === "" ? "anon" : state.name}-F_Mask.gbr`, frontMask.toString() );
           break;
         case "B.Mask":
-          // To make this possible we need to get the BACK layer pads and offset them a bit.
+          let backMask = new GerberBuilder();
+          backMask.plotPads(layers["B.Cu"], 0.1);
+          if (global_state.downloadGerberOptions.includeEdgeCuts) {
+            backMask.plotOutline(layers["interior"]);
+          }
+          zip.file(`${state.name === "" ? "anon" : state.name}-B_Mask.gbr`, backMask.toString() );
           break;
         case "F.Silkscreen":
           // It would be good to have more options in terms of element placement here.
@@ -338,7 +299,9 @@ export function downloadGerber(state) {
           // It would be good to have more options in terms of element placement here.
           break;
         case "Edge.Cuts":
-          zip.file(`${state.name === "" ? "anon" : state.name}-Edge_Cuts.gbr`, makeFileEdgeCuts(interior));
+          let outline = new GerberBuilder();
+          outline.plotOutline(layers["interior"]);
+          zip.file(`${state.name === "" ? "anon" : state.name}-Outline.gbr`, outline.toString() );
           break;
         case "Drills":
           // There is probably no need to include edge cuts in the drill file 
