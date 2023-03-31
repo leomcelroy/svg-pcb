7204d02b8ff4fda0a47116e9650e257ac5dfafb9
leomcelroy <leomcelroy@gmail.com>
2023-03-30 20:16:11 -0400

    Update dispatch.js

--- js/dispatch.js
+++ js/dispatch.js
@@ -73,8 +73,6 @@ const ACTIONS = {
 
 			string = modifyAST(string, changes);
 
-			console.log(string);
-
 		  const included = makeIncluded(flatten);
 		  
 			const f = new Function(...Object.keys(included), string)
