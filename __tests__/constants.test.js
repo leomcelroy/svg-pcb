2d1571c5244d2d6d0819b1c6de1ba13acce3f56d
Krisjanis Rijnieks <krisjanis.rijnieks@gmail.com>
2023-04-02 17:50:57 +0300

    Add jest tests with two example tests

---
+++ __tests__/constants.test.js
@@ -0,0 +1,5 @@
+import { MM_PER_INCH } from '../js/constants';
+
+test('MM_PER_INCH should be 25.4', () => {
+  expect(MM_PER_INCH).toBe(25.4);
+});
--- js/pcb_helpers.js
+++ js/pcb_helpers.js
@@ -7,7 +7,7 @@ import {
   rotate as rot, 
   outline,
   scale
-} from "/geogram/index.js";
+} from "../geogram/index.js";
 
 const length = ([x1, y1], [x2, y2]) => Math.sqrt((x2-x1)**2 + (y2-y1)**2);
 
