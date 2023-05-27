4ec6ee435c2636ca1a997035e98ee1d8af65adc6
leomcelroy <leomcelroy@gmail.com>
2023-05-26 23:45:49 -0400

    Update ki_cad_parser.js

--- js/ki_cad_parser.js
+++ js/ki_cad_parser.js
@@ -10,7 +10,14 @@ const convertLayers = (layers) => layers.reduce((acc, cur) => {
   if (l.length === 0) return acc;
   else if (l[0] !== "*") return [...acc, cur];
   else return [...acc, `F.${l[1]}`, `B.${l[1]}`];
-}, [])
+}, []);
+
+const getNamedArray = (line, name) => {
+  const index = line.findIndex(entry => Array.isArray(entry) && entry[0] === name);
+  const value = line[index];
+
+  return value ? value.slice(1) : [];
+}
 
 export function kicadToObj(data) {
   let r = sParse(data);
@@ -27,26 +34,38 @@ export function kicadToObj(data) {
     if (isPad && (isSmd || isThru)) {
       let name = line[1];
 
-      let at = line[4].slice(1).map(x => Number(x)*scale);
+      let at = getNamedArray(line, "at").map(x => Number(x)*scale);
       at[1] = -at[1]; // negative Y axis
 
-      const layersIndex = line.findIndex(entry => Array.isArray(entry) && entry[0] === "layers");
-      let layers = line[layersIndex];
-      layers = layers ? layers.slice(1) : [];
+      let layers = getNamedArray(line, "layers");
       layers = convertLayers(layers);
 
-  
-      let size = line[5].slice(1).map(x => Number(x)*scale);
+      let size = getNamedArray(line, "size").map(x => Number(x)*scale);
 
       const shapeCases = {
         "rect": () => rectangle(...size),
         "roundrect": () => {
-          let _ = rectangle(...size);
+          // let _ = rectangle(...size);
           // roundCorners(_);
-          return _;
+
+          const ratio = Number(getNamedArray(line, "roundrect_rratio")[0]);
+          const pts = generateRoundRect(0, 0, ...size, ratio);
+
+          return [ pts ];
         },
         "circle": () => circle(...size.map(x => x/2)),
         "oval": () => circle(...size.map(x => x/2)),
+        "custom": () => {
+          const primitives = getNamedArray(line, "primitives");
+
+          const poly = primitives[0][1]
+            .slice(1)
+            .map(
+              xy => [ Number(xy[1])*scale, Number(xy[2])*scale ]
+            );
+
+          return [ poly ];
+        }
         // "ellipse": () => circle(...size), 
       }
 
@@ -110,6 +129,53 @@ export function kicadToObj(data) {
   return result;
 }
 
+
+function generateRoundRect(centerX, centerY, width, height, rratio, numPointsPerCorner=10) {
+    // Calculate the radius
+    var radius = Math.min(width, height) * rratio / 2;
+
+    // Calculate the corner centers
+    var topLeft = [centerX - width/2 + radius, centerY - height/2 + radius];
+    var topRight = [centerX + width/2 - radius, centerY - height/2 + radius];
+    var bottomRight = [centerX + width/2 - radius, centerY + height/2 - radius];
+    var bottomLeft = [centerX - width/2 + radius, centerY + height/2 - radius];
+
+    var points = [];
+
+    // Generate points for each corner, we're using a simple circle equation here.
+    for (var i = 0; i < numPointsPerCorner; i++) {
+        var angle = Math.PI / 2 * i / (numPointsPerCorner - 1);
+
+        // Top left corner
+        points.push([topLeft[0] - radius * Math.cos(angle), topLeft[1] - radius * Math.sin(angle)]);
+    }
+
+    for (var i = 0; i < numPointsPerCorner; i++) {
+        var angle = Math.PI / 2 * i / (numPointsPerCorner - 1);
+
+        // Top right corner
+        points.push([topRight[0] + radius * Math.sin(angle), topRight[1] - radius * Math.cos(angle)]);
+    }
+
+    for (var i = 0; i < numPointsPerCorner; i++) {
+        var angle = Math.PI / 2 * i / (numPointsPerCorner - 1);
+
+        // Bottom right corner
+        points.push([bottomRight[0] + radius * Math.cos(angle), bottomRight[1] + radius * Math.sin(angle)]);
+    }
+
+    for (var i = 0; i < numPointsPerCorner; i++) {
+        var angle = Math.PI / 2 * i / (numPointsPerCorner - 1);
+
+        // Bottom left corner
+        points.push([bottomLeft[0] - radius * Math.sin(angle), bottomLeft[1] + radius * Math.cos(angle)]);
+    }
+
+    // Add first point to the end to close the shape
+    points.push(points[0]);
+
+    return points;
+}
 
 
 
